73a80ef2b181837d0c6a0bba119274b2c6444c2a
romjanali1 <mdromgana@gmail.com>
2022-04-15 03:48:39 +0600

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Pages/Footer/Footer';
+import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>my new project</h1>
-      <h2>test</h2>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/'element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
