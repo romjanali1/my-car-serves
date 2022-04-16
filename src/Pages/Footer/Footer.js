a6fa619d701eb612e054f0eb1a8e87a85ec8ae6d
romjanali1 <mdromgana@gmail.com>
2022-04-16 12:02:10 +0600

    footer

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import logo from '../../photos/logo.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>its footer</h1>
+        <div  className='row text-white bg-dark p-5'>
+           <div className='col-6'> <img className='w-50' src={logo} alt="" /></div>
+            <div className='col-3'>
+                <p>About Online Food</p>
+                <p>Read Our blog</p>
+                <p>Sign up deliver</p>
+                <p>Add your restaurant</p>
+            </div>
+            <div className='col-3'>
+                <p>Get help</p>
+                <p>Read FAQs</p>
+                <p>View all cities</p>
+                <p>Restaurants near me</p>
+            </div>
+            <div className='row'>
+                <div className='col-6'><p>Copyright @ 2020 Online Food</p></div>
+                <div className='col-6 d-flex p-2 gx-2'>
+                <p>Privacy Policy</p>
+                <p>Terms of Use</p>
+                <p>Pricing</p>
+                </div>
+            </div>
         </div>
     );
 };
