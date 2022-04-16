3a5253a9c9a2ee129b7c15233bd64661583adeb6
romjanali1 <mdromgana@gmail.com>
2022-04-16 16:34:37 +0600

    header

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import mylogo from '../../photos/logo2.png';
 
 const Header = () => {
     return (
         <div>
-            <h1>its header</h1>
+    <Navbar variant="light">
+    <Container>
+    <Navbar.Brand href="#home">
+      <img
+        src={mylogo}
+        className="w-25"
+        alt=""
+      />
+    </Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="/login">Login</Nav.Link>
+      <Nav.Link href="/sing up" className='rounded-pill bg-danger text-white px-4'>Sing up</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
         </div>
     );
 };
